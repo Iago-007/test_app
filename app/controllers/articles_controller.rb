20eb8f5f0169888f4e7ff49cafb3912f9a8af47e
Iago-007 <zahidbashir020@gmail.com>
2020-05-12 01:34:30 +0500

    create new article or insert record in DB

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,17 @@
 class ArticlesController < ApplicationController
     def show
-        byebug
+        # byebug
         @article = Article.find(params[:id])
+    end
+    def index
+        @articles=Article.all
+    end
+    def new
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        # render plain: @article.inspect
+        @article.save
+        redirect_to article_path(@article)
     end
 end
