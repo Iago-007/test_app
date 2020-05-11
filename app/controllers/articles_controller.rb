726aeff049e6be797c343a791fbdede2b4833d8a
Iago-007 <zahidbashir020@gmail.com>
2020-05-12 01:59:48 +0500

    Diplaying flash messgaes and errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,11 +7,16 @@ class ArticlesController < ApplicationController
         @articles=Article.all
     end
     def new
+        @article = Article.new
     end
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         # render plain: @article.inspect
-        @article.save
-        redirect_to article_path(@article)
+        if @article.save
+            flash[:notice]="Article was created successfully."
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
 end
