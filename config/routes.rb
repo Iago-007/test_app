d61f600b11c02505538a4d36fe385930d41f1a7d
Iago-007 <zahidbashir020@gmail.com>
2020-05-13 00:54:43 +0500

    Add Layout Links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#hello'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  # above is the same as below..above we have seen all he http routes
+  resources :articles
 end
